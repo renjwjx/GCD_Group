c9d3c2a9dfaf0a83a28ec5a2eca4c27a171050cd
Jinwei <renjwjx@gmail.com>
2021-01-02 22:19:54 +0800

    gcd group

--- GCD_Group/ViewController.m
+++ GCD_Group/ViewController.m
@@ -19,5 +19,30 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self dispatchGroup];
+}
+
+- (void)dispatchGroup
+{
+    dispatch_queue_t queue = dispatch_queue_create("GCD group", DISPATCH_QUEUE_PRIORITY_DEFAULT);
+    
+    dispatch_group_t group = dispatch_group_create();
+    
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"group1 task");
+    });
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"group2 task");
+    });
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"group3 task");
+    });
+    //group task complete, then notify 
+    dispatch_group_notify(group, queue, ^{
+        NSLog(@"group notify end");
+    });
+}
 
 @end
